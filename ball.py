c62bd07dbd13d54c9906b11c89858331674ae3e5
Gabriel Porto <portoduque@outlook.com>
2021-04-23 13:38:09 -0300

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,57 @@
+from turtle import Turtle
+
+INCREASED_SPEED = 0.02
+DEFAULT_SPEED = 0.15
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("white")
+        self.x_move_distance = DEFAULT_SPEED
+        self.y_move_distance = DEFAULT_SPEED
+
+    def move(self):
+        new_x = self.xcor() + self.x_move_distance
+        new_y = self.ycor() + self.y_move_distance
+        self.goto(new_x, new_y)
+
+    def wall_bounce(self):
+        self.y_move_distance *= -1
+
+    def paddle_bounce(self):
+        self.x_move_distance *= -1
+        self.increase_speed()
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.reset_speed()
+        self.paddle_bounce()
+
+    def increase_speed(self):
+        if self.x_move_distance > 0:
+            self.x_move_distance += INCREASED_SPEED
+        else:
+            self.x_move_distance -= INCREASED_SPEED
+
+        if self.y_move_distance > 0:
+            self.y_move_distance += INCREASED_SPEED
+        else:
+            self.y_move_distance -= INCREASED_SPEED
+
+    def reset_speed(self):
+        if self.x_move_distance > 0:
+            self.x_move_distance = DEFAULT_SPEED
+        else:
+            self.x_move_distance = DEFAULT_SPEED
+
+        if self.y_move_distance > 0:
+            self.y_move_distance = DEFAULT_SPEED
+        else:
+            self.y_move_distance = DEFAULT_SPEED
+
+
+
+
